d61d5449a65348062cb9e49c4b82611b96b711b6
isaacbello <isaacbello3200@gmail.com>
2024-06-11 00:08:24 +0100

    turn off no display rule

--- src/app/app/trees/components/tree-list.tsx
+++ src/app/app/trees/components/tree-list.tsx
@@ -55,7 +55,7 @@ export default function TreesList() {
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full mt-10">
                     {sortedTrees?.map(tree => (
-                        <Tree tree={tree} editFunction={(tree) => {
+                        <Tree key={tree.id} tree={tree} editFunction={(tree) => {
                             setIsModalOpen(true)
                             setSelectedTree({
                                 ...tree,
